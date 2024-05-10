27ecf84f933b4aaaeb07103b6f79d3f063e0b0bb
NofarNatiga <150009506+NofarNatiga@users.noreply.github.com>
2024-05-10 13:59:39 +0300

    initial commit

--- client_folder/try1.py
+++ client_folder/try1.py
@@ -1,99 +1,182 @@
-from types import NoneType
-
-import flet as ft
-
-from Requests_2 import Client
+import requests
+import json
+from fastapi import UploadFile
 
 
-def handle_tap(e):
-    print(f"handle_tap")
+class Client:
+    def __init__(self, url):
+        self.name = None
+        self.password = None
+        self.token = None  # Add a token attribute to store the JWT token
+        self.server_address = url
 
-
-class Works:
-    def __init__(self, page: ft.Page, client: Client):
-        self.page = page
-        self.page.vertical_alignment = ft.alignment.top_left
-        self.client = client
-        self.list1 = None
-        self.image_data_info_list = None
-        self.grid = None
-        self.y = None
-
-        self.anchor = ft.SearchBar(
-            view_elevation=4,
-            divider_color=ft.colors.AMBER,
-            bar_hint_text="Search Work...",
-            on_tap=handle_tap,
-            width=300,
-            on_submit=self.handle_submit
-        )
-        list1 = client.get_all("categories")
-        self.anchor.controls = [
-            ft.ListTile(title=ft.Text(f" {i}"), on_click=self.close_anchor, data=i, )
-            for i in list1
-        ]
-
-        self.row = ft.Row(alignment=ft.MainAxisAlignment.CENTER,
-                          controls=[
-                              ft.IconButton(icon=ft.icons.SEARCH,
-                                            on_click=lambda _: self.anchor.open_view()
-                                            ),
-                          ], )
-
-        self.for_search = ft.Row([
-            self.anchor,
-            self.row, ], spacing=30, alignment=ft.MainAxisAlignment.CENTER)
-
-        self.empty_space = ft.Container(height=10)
-        self.page.add(self.empty_space, self.for_search, self.empty_space)
-
-    def close_anchor(self, e):
-        text = f"{e.control.data}"
-        print(text)
-        print(f"closing view from {text}")
-
-        if self.grid is None:
-            self.grid = self.create_images_grid(self.image_data_info_list)
-        else:
-            self.page.remove(self.grid)
-            self.grid = self.create_images_grid(self.image_data_info_list)
-        self.page.add(self.grid)
-
-        self.page.update()
-
-        self.anchor.close_view(text)
-
-    def handle_submit(self, e):
-        text = f"{e.data}"
-        print("handle submit:", text)
-        print(f"closing view from {text}")
-
-        if self.grid is None:
-            self.grid = self.create_images_grid(self.image_data_info_list)
-        else:
-            self.page.remove(self.grid)
-            self.grid = self.create_images_grid(self.image_data_info_list)
-        self.page.add(self.grid)
-
-        self.page.update()
-
-    def create_images_grid(self, image_data_info_list) -> ft.GridView:
-        images_grid = ft.GridView(
-            expand=1,
-            runs_count=1,
-            max_extent=275,
-            child_aspect_ratio=1.0,
-            spacing=20,
-            run_spacing=20,
-            width=1300,
+    def authenticate(self, name, password):
+        credentials = {"name": name, "password": password}
+        response_ = requests.post(
+            f"{self.server_address}/authenticate",
+            json=credentials
         )
 
-        for image_info in image_data_info_list:
-            if image_info["type"] == "image":
-                images_grid.controls.append(
-                    ft.Container(
-                        width=270, height=270,
-                         on_click=self.clicked(image_info),
-                    )
-                )
-        return images_grid
+        response_json = response_.json()
+        print(response_, response_json)
+
+        if response_json["response"].upper() == "USER AUTHENTICATED":
+            self.name = name
+            self.password = password
+            self.token = response_json.get("access_token")  # Store the token
+
+        return response_, response_json
+
+    def signout(self):
+        headers = {
+            "Authorization": f"Bearer {self.token}"
+        }
+        response_ = requests.post(
+            f"{self.server_address}/signout",
+            headers=headers
+        )
+        return response_, response_.json()
+
+    def add_work(self, work_data: dict):
+        headers = {
+            "Authorization": f"Bearer {self.token}"
+        }
+        data_ = {"work": work_data}
+        response_ = requests.post(
+            f"{self.server_address}/add_work",
+            json=data_,
+            headers=headers
+        )
+        return response_, response_.json()
+
+    def add_work_video(self, work_data: dict, video_binary_data: bytes, file_type: str):
+        headers = {
+            "Authorization": f"Bearer {self.token}"
+        }
+        url = f"{self.server_address}/add_work_video"
+        files_ = {
+            "video_file": (f"video.{file_type}", video_binary_data, file_type)
+        }
+        form_data = {
+            "title": work_data["title"],
+            "category_names": work_data["category_names"],
+            "rating": work_data["rating"],
+            "description": work_data["description"],
+            "public": work_data["public"],
+            "date": work_data["date"]
+        }
+
+        response_ = requests.post(
+            url,
+            params=form_data,
+            files=files_,
+            headers=headers
+        )
+        return response_, response_.json()
+
+    def add_category(self, category_data: dict):
+        headers = {
+            "Authorization": f"Bearer {self.token}"
+        }
+        data_ = {"category": category_data}
+        response_ = requests.post(
+            f"{self.server_address}/add_category",
+            json=data_,
+            headers=headers
+        )
+        return response_, response_.json()
+
+    def signup(self, name, password):
+        credentials = {"name": name, "password": password}
+        response_ = requests.post(
+            f"{self.server_address}/signup",
+            json=credentials
+        )
+
+        response_json = response_.json()
+        print(response_json)
+
+        if response_json["response"].upper() == "SIGNUP SUCCESS":
+            self.name = name
+            self.password = password
+
+        return response_, response_json
+
+    def get_all(self, what: str):
+        headers = {
+            "Authorization": f"Bearer {self.token}"
+        }
+        url = f"{self.server_address}/for_get_all"
+        params = {"what": what}
+        response_ = requests.get(url, params=params, headers=headers)
+
+        print(response_.json())
+        print(response_)
+
+        return response_, response_.json()
+
+    def get_all_public(self, what: str):
+        url = f"{self.server_address}/get_all_public"
+        params = {"what": what}
+        response_ = requests.get(url, params=params)
+
+        print(response_.json())
+        print(response_)
+
+        return response_, response_.json()
+
+    def get_images(self, value: dict):
+        headers = {
+            "Authorization": f"Bearer {self.token}"
+        }
+        url = f"{self.server_address}/for_get_images"
+        params = value
+        response_ = requests.get(url, params=params, headers=headers)
+
+        return response_, response_.json()
+
+    def get_images_public(self, value: dict):
+        url = f"{self.server_address}/public_works"
+        params = value
+        response_ = requests.get(url, params=params)
+
+        return response_, response_.json()
+
+    def update_work_details(self, work_title: str, work_details: dict):
+        headers = {
+            "Authorization": f"Bearer {self.token}"
+        }
+        data_ = {"work_details": work_details}
+        response_ = requests.post(
+            f"{self.server_address}/update_work_details",
+            json=data_,
+            params={"work_title": work_title},
+            headers=headers
+        )
+        return response_, response_.json()
+
+    def delete_work(self, work_name):
+        headers = {
+            "Authorization": f"Bearer {self.token}"
+        }
+        response_ = requests.post(
+            f"{self.server_address}/delete_work/{work_name}",
+            headers=headers
+        )
+        return response_, response_.json()
+
+    def video(self, title):
+        headers = {
+            "Authorization": f"Bearer {self.token}"
+        }
+        response_ = requests.get(
+            f"{self.server_address}/video_url/{title}",
+            headers=headers
+        )
+
+        if response_.status_code == 200:
+            return response_, response_.json()
+        else:
+            # Handle non-successful responses (e.g., 401, 404, etc.)
+            print(f"Failed to fetch video: HTTP {response_.status_code} - {response_.text}")
+            response_.raise_for_status()  # Raise an HTTPError for non-successful responses
